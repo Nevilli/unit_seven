b71357bff8b80d4dfe4d67898218252964a53fc0
Lliam <lliam@iMac8.local>
2018-11-02 11:20:37 -0400

    Finished all code

--- Documents/unit_seven/assignment_seven.py
+++ Documents/unit_seven/assignment_seven.py
@@ -1,20 +1,56 @@
+# Liam Neville
+# 10/2/18
+# This program allows the user to encode and decode a message
+
+
 def choose():
+    """
+    This function
+    :return:
+    """
     choice1 = input("Type e to encode, type d to decode, or type q to quit")
     if choice1 == "e":
-        print("encode")
+        return "e"
     elif choice1 == "d":
-        print("decode")
+        return "d"
     else:
         print("Goodbye")
 
 
 def main():
-    if choose == "e":
+    choice = choose()
+    if choice == "e":
         number = int(input("Choice a number between 0-25"))
+        phrase = input("Choose a phrase to encode")
         alphabet = "abcdefghijklmnopqrstuvwxyz"
         first = alphabet[0:number]
         last = alphabet[number:]
         final = last + first
+        encode = ""
+        for x in phrase:
+            if x not in alphabet:
+                encode = encode + x
+            else:
+                code = alphabet.index(x)
+                final2 = final[code]
+                encode = encode + final2
+        print(encode)
+    elif choice == "d":
+        number = int(input("Choice a number between 0-25"))
+        phrase = input("Choose a phrase to decode")
+        alphabet = "zyxwvutsrqponmlkjihgfedcba"
+        first = alphabet[0:number]
+        last = alphabet[number:]
+        final = last + first
+        decode = ""
+        for x in phrase:
+            if x not in final:
+                decode = decode + x
+            else:
+                code = alphabet.index(x)
+                final2 = final[code]
+                decode = decode + final2
+        print(decode)
 
 
 main()
